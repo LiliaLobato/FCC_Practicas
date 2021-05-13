d15a46ae425bd0c98f4d702be7b9f2fee7a7c16d
LiliaLobato <ie706937@iteso.mx>
2021-05-13 02:35:41 -0500

    fixed some errors

--- P4/RelacionesFunciones.py
+++ P4/RelacionesFunciones.py
@@ -38,6 +38,7 @@ for i in range(transiciones):
 		isReflexivo=isReflexivo+1
 		#print(transicion_1)
 
+
 if (isReflexivo) >= num_nodos:
 	print("############ ES REFLEXIVO ###############")
 else :
@@ -58,20 +59,20 @@ for i in range(transiciones):
 			if (node1_0 != node2_0) and (node1_1 != node2_1): #valida que no sea el mismo
 				if (node1_0 == node2_1) and (node1_1 == node2_0): #valida que sea inverso
 					isSimetrico=isSimetrico+1
-					#print(transicion_1,transicion_2)
-		else:
-			isSimetrico=isSimetrico+1
+					print(transicion_1,transicion_2)
+		#else:
+			#isSimetrico=isSimetrico+1
 			#print(transicion_1,transicion_2)
 
-if isSimetrico%num_nodos == 0:
+if isSimetrico%num_nodos == 0 and isSimetrico != 0:
 	print("############ ES SIMETRICO ###############")
 else:
 	print("########### NO ES SIMETRICO ##############")
 	
 
-########## REVISAR SI ES SIMETRICO
+########## REVISAR SI ES TRANSITIVO
 
-isTransitivo = True
+isTransitivo = 0
 
 for i in range(transiciones):
 	transicion_1 = (Relacion[i])
@@ -92,14 +93,22 @@ for i in range(transiciones):
 				#obtnemos x,z
 				node2_x = transicion_3[0]
 				node2_z = transicion_3[1]
-				if(node1_x == node2_x) and (node1_z == node2_z) and (node1_y == node2_y): #Validamos que X y Z sean los mismos
+				if(node1_x == node2_x) and (node1_z == node2_z): #Validamos que X y Z sean los mismos
 					node_x = node2_x
 					node_z = node2_z
-					#print(node_x, node_y, node_z)
-				else:
-					isTransitivo = False
+					if (str(node_x), str(node_z)) in Relacion:
+						print("Los nodos: x=",node_x, " y=",node_y, " z=", node_z, "son transitivos")
+						isTransitivo = isTransitivo +1
+					else:
+						isTransitivo = 0
+						print("Los nodos: x=",node_x, " y=",node_y, " z=", node_z, "NO EXISTE")
+					#print(node2_x,node1_x)
+					#print(node2_y,node1_y)
+					#print(node2_z,node1_z)
+
+
 					
-if isTransitivo == True:
+if isTransitivo == transiciones:
 	print("############ ES TRANSITIVO ###############")
 else:
 	print("########### NO ES TRANSITIVO ##############")
@@ -127,10 +136,14 @@ print ("CODOMINIO: ", codominio)
 ########## ES FUNCIÓN?
 isFuncion = True;
 for i in range (transiciones-1):
-	if len(Relacion[i]) == 1 or len(Relacion[i+1]) == 1:
-		isFuncion = False
-	if Relacion[i][0] == Relacion[i+1][0]:
-		isFuncion = False
+	for j in range (0, len(Relacion)-1):
+		if (i != j) and (Relacion[i] == Relacion[j]):
+			isFuncion = True
+		else:
+			if len(Relacion[i]) == 1 or len(Relacion[i+1]) == 1:
+				isFuncion = False
+			if Relacion[i][0] == Relacion[i+1][0]:
+				isFuncion = False
 
 if isFuncion == True:
 	print("############ ES FUNCIÓN ###############")
