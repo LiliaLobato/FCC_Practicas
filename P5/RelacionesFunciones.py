b1b51b9b6d1861e3f3d6882cf3e5d989e82f85c0
LiliaLobato <ie706937@iteso.mx>
2021-05-13 08:51:48 -0500

    cambios en revision

--- P5/RelacionesFunciones.py
+++ P5/RelacionesFunciones.py
@@ -22,6 +22,7 @@ Relacion.pop(len(Relacion)-1)
 print("\n############### RELACION ###############")
 print("NODOS: ",Nodos)
 print("RELACION: ",Relacion)
+print("\n##############################\n")
 
 num_nodos = len(Nodos)
 transiciones = len(Relacion)
@@ -36,43 +37,37 @@ for i in range(transiciones):
 	node1_1 = transicion_1[1]
 	if (node1_0 == node1_1): #validamos que sea un loop
 		isReflexivo=isReflexivo+1
+		print("Los nodos: ",transicion_1, " EXISTE")
 		#print(transicion_1)
 
 
 if (isReflexivo) >= num_nodos:
-	print("############ ES REFLEXIVO ###############")
+	print("############ ES REFLEXIVO ###############\n")
 else :
-	print("########### NO ES REFLEXIVO ##############")
+	print("########### NO ES REFLEXIVO ##############\n")
 
 
 ########## REVISAR SI ES SIMETRICO
-isSimetrico = 0
+isSimetrico = 1
 for i in range(transiciones):
 	transicion_1 = (Relacion[i])
 	node1_0 = transicion_1[0]
 	node1_1 = transicion_1[1]
-	for j in range(transiciones):
-		transicion_2 = (Relacion[j])
-		node2_0 = transicion_2[0]
-		node2_1 = transicion_2[1]
-		if (node1_0 != node1_1): #validamos que no sea un loop
-			if (node1_0 != node2_0) and (node1_1 != node2_1): #valida que no sea el mismo
-				if (node1_0 == node2_1) and (node1_1 == node2_0): #valida que sea inverso
-					isSimetrico=isSimetrico+1
-					print(transicion_1,transicion_2)
-		#else:
-			#isSimetrico=isSimetrico+1
-			#print(transicion_1,transicion_2)
+	if (str(node1_1), str(node1_0)) in Relacion:
+		print("Los nodos:",transicion_1, "tiene inverso")
+	else:
+		print("Los nodos:",transicion_1, "NO tiene inverso")
+		isSimetrico = 0
 
-if isSimetrico%num_nodos == 0 and isSimetrico != 0:
-	print("############ ES SIMETRICO ###############")
+if isSimetrico == 1:
+	print("############ ES SIMETRICO ###############\n")
 else:
-	print("########### NO ES SIMETRICO ##############")
+	print("########### NO ES SIMETRICO ##############\n")
 	
 
 ########## REVISAR SI ES TRANSITIVO
 
-isTransitivo = 0
+isTransitivo = 1
 
 for i in range(transiciones):
 	transicion_1 = (Relacion[i])
@@ -87,31 +82,17 @@ for i in range(transiciones):
 
 		if(node1_y == node2_y): #validamos que ambas Y sean las mismas
 			node_y = node2_y
-
-			for k in range(transiciones):
-				transicion_3 = (Relacion[k])
-				#obtnemos x,z
-				node2_x = transicion_3[0]
-				node2_z = transicion_3[1]
-				if(node1_x == node2_x) and (node1_z == node2_z): #Validamos que X y Z sean los mismos
-					node_x = node2_x
-					node_z = node2_z
-					if (str(node_x), str(node_z)) in Relacion:
-						print("Los nodos: x=",node_x, " y=",node_y, " z=", node_z, "son transitivos")
-						isTransitivo = isTransitivo +1
-					else:
-						isTransitivo = 0
-						print("Los nodos: x=",node_x, " y=",node_y, " z=", node_z, "NO EXISTE")
-					#print(node2_x,node1_x)
-					#print(node2_y,node1_y)
-					#print(node2_z,node1_z)
-
-
+			if (str(node1_x), str(node1_z)) in Relacion:
+				print("Los nodos: x=",node1_x, " y=",node_y, " z=", node1_z, "son transitivos")
+				#isTransitivo = 1
+			else:
+				isTransitivo = 0
+				print("Los nodos: x=",node1_x, " y=",node_y, " z=", node1_z, "NO EXISTE")
 					
-if isTransitivo == transiciones:
-	print("############ ES TRANSITIVO ###############")
+if isTransitivo:
+	print("############ ES TRANSITIVO ###############\n")
 else:
-	print("########### NO ES TRANSITIVO ##############")
+	print("########### NO ES TRANSITIVO ##############\n")
 	
 
 ########## CALCULAR DOMINIO
@@ -150,6 +131,6 @@ for i in range (0, len(Relacion)-1):
                         isFuncion = False
 
 if isFuncion == True:
-	print("############ ES FUNCIÓN ###############")
+	print("############ ES FUNCIÓN ###############\n")
 else:
-	print("########### NO ES FUNCIÓN ##############")
+	print("########### NO ES FUNCIÓN ##############\n")
